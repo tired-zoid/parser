07918021bbfe00e38a3f95485fea69554a1f9b85
tired-zoid <rahimzoid@gmail.com>
2026-05-15 12:39:52 +0300

    add LogImportService.php with db insert

--- commands/LogController.php
+++ commands/LogController.php
@@ -2,26 +2,14 @@
 
 namespace app\commands;
 
+use app\services\LogImportService;
 use yii\console\Controller;
-use UAParser\Parser;
 
 class LogController extends Controller
 {
     public function actionImport($path)
     {
-        $parser = Parser::create();
-        $result = $parser->parse($userAgent);
-        $browser = $result->ua->family;
-        $os = $result->os->family;
-        $architecture = str_contains($userAgent, 'x86_64') ? 'x64' : 'x86';
-        $handle = fopen($path, 'r');
-
-        while (($line = fgets($handle)) !== false) {
-
-            var_dump($line);
-
-        }
-
-        fclose($handle);
+        $service = new LogImportService();
+        $service->import($path);
     }
 }
---
+++ services/LogImportService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\services;
+
+use Yii;
+
+class LogImportService
+{
+    private ParserService $parser;
+
+    public function __construct()
+    {
+        $this->parser = new ParserService();
+    }
+    public function import(string $path): void
+    {
+        $handle = fopen($path, 'r');
+        $batch = [];
+
+        while (($line = fgets($handle)) !== false) {
+            $data = $this->parser->parseLine($line);
+
+            if (!$data) continue;
+
+            $batch[] = $data;
+
+            if (count($batch) >= 1000) {
+                $this->flush($batch);
+                $batch = [];
+            }
+        }
+
+        if ($batch) {
+            $this->flush($batch);
+        }
+
+        fclose($handle);
+    }
+
+    private function flush(array $batch)
+    {
+        Yii::$app->db->createCommand()
+            ->batchInsert('logs',
+                ['ip','requested_at','url','user_agent','os','architecture','browser'],
+                $batch)
+            ->execute();
+    }
+
+}
